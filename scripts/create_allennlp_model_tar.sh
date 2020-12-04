a519a6bc98c8e57e5c1b7c4e2cdb7abc630c68ff
David Wilmot <david.wilmot@ed.ac.uk>
2020-12-04 13:17:39 +0000

    Manually compress model tar.

---
+++ scripts/create_allennlp_model_tar.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env
+
+SERIALIZATION_DIR=${SERIALIZATION_DIR}
+WEIGHTS_TH="best.th"
+
+MODEL_ARCHIVE=${SERIALIZATION_DIR}/model.tar.gz
+
+if [ -f "${MODEL_ARCHIVE}" ]; then
+    echo "Model archive exists: ${MODEL_ARCHIVE}"
+    exit 1
+fi
+
+echo "Making model.tar.gz in ${MODEL_ARCHIVE}"
+
+cd ${SERIALIZATION_DIR}
+
+if [ -d "modeltar" ]; then
+    echo "modeltar directory exists. Deleting ... "
+    rm -r modeltar
+fi
+
+mkdir modeltar
+cp -r vocabulary modeltar/
+cp ${WEIGHTS_TH} modeltar/weights.th
+cp config.json modeltar/
+cd modeltar
+tar -czvf model.tar.gz ./*
+cp model.tar.gz ../
+cd ../
+rm -r modeltar
