ba7379b7f03f605736dd57db31d0f0d5b16124f7
David Wilmot <david.wilmot@ed.ac.uk>
2021-01-16 00:12:12 +0000

    Export to dataset slurm wrapper.

---
+++ slurm/export_dataset_to_json.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#SBATCH -o /home/%u/slurm_logs/slurm-%A_%a.out
+#SBATCH -e /home/%u/slurm_logs/slurm-%A_%a.out
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --gres=gpu:1
+#SBATCH --mem=64g  # Memory
+#SBATCH --cpus-per-task=12  # number of cpus to use - there are 32 on each node.
+
+# Set EXP_BASE_NAME and BATCH_FILE_PATH
+
+echo "============"
+echo "Initialize Env ========"
+
+set -e # fail fast
+
+export CURRENT_TIME=$(date "+%Y_%m_%d_%H%M%S")
+
+# Activate Conda
+source /home/${USER}/miniconda3/bin/activate allennlp
+
+cd /home/s1569885/git/story-fragments/story_fragments/data_processing 
+
+python export_dataset_to_json.py export --script-path ${SCRIPT_PATH} \
+--dataset-name ${DATASET_NAME} --output-file ${OUTPUT_FILE} --split ${DATASET_SPLIT} \
+--nested ${NESTED}
+
+
