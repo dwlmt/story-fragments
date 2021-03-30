d07cf5e4a512b999641c99609e0fc0ea8806e3c2
David Wilmot <david.wilmot@ed.ac.uk>
2021-03-30 13:20:25 +0100

    Fixes to clustering.

--- story_fragments/data_processing/cluster_stories.py
+++ story_fragments/data_processing/cluster_stories.py
@@ -3,7 +3,7 @@
 import collections
 import textwrap
 from pathlib import Path
-from typing import List
+from typing import List, OrderedDict
 
 import fire
 import jsonlines
@@ -136,7 +136,7 @@ class ClusterStories(object):
                         story_field_df_list = []
 
                         if "title" in obj:
-                            story_id = obj["title"]
+                            story_id = obj["title"].replace(".txt","")
                         else:
                             story_id = f"{i}"
 
@@ -273,6 +273,234 @@ class ClusterStories(object):
             y_edges.extend([y_points[s], y_points[t], None])
         return x_edges, y_edges
 
+    def network_memory(self,
+                src_json: List[str],
+                output_dir: str,
+                num_central_nodes_to_plot: int = 50,
+                centrality_depth_limit: int = 1
+                ):
+        # print(f"Params: {src_json}", {output_dir}, {plot_fields}, {plot_field_names})
+
+        Path(output_dir).mkdir(parents=True, exist_ok=True)
+
+        if isinstance(src_json, str):
+            src_json = [src_json]
+
+            i = 0
+
+            for json_file in src_json:
+                # print(f"Process: {json_file}")
+
+                with jsonlines.open(json_file) as reader:
+
+                    for obj in reader:
+
+                        if "title" in obj:
+                            story_id = obj["title"].replace(".txt","")
+                        else:
+                            story_id = f"{i}"
+
+                        Path(f"{output_dir}/{story_id}/memory_network/").mkdir(parents=True, exist_ok=True)
+
+                        if "passages" in obj:
+
+                            G = self.construct_graph(obj, story_id)
+
+
+                            self.add_centrality(G, nx.degree_centrality(G), "degree_centrality")
+                            try:
+                                self.add_centrality(G, nx.eigenvector_centrality(G, weight="weight"), "eigenvector_centrality")
+                            except:
+                                pass
+                            self.add_centrality(G, nx.closeness_centrality(G), "closeness_centrality")
+                            self.add_centrality(G, nx.information_centrality(G, weight="weight"),
+                                                                    "information_centrality")
+                            self.add_centrality(G, nx.betweenness_centrality(G, weight="weight"), "betweenness_centrality")
+                            sorted_centrality = self.add_centrality(G, nx.current_flow_betweenness_centrality(G, weight="weight"),
+                                                "betweenness_current_centrality")
+
+                            pos = nx.spring_layout(G)#nx.multipartite_layout(G, align="horizontal")
+                            print(pos)
+
+                            plot_name = "memory_network"
+                            fig = self._plot_memory_network(G, pos)
+                            plotly.io.write_html(fig=fig, file=f"{output_dir}/{story_id}/memory_network/{plot_name}.html",
+                                                 include_plotlyjs='cdn',
+                                                 include_mathjax='cdn', auto_open=False)
+
+                            nx.write_yaml(G, f"{output_dir}/{story_id}/memory_network/memory_network.yaml")
+
+                            cent_counter = 0
+                            for j, (node, centrality) in enumerate(sorted_centrality.items()):
+
+                                if cent_counter == num_central_nodes_to_plot:
+                                    continue
+
+                                successors = nx.bfs_successors(G, source=node, depth_limit=centrality_depth_limit)
+                                predecessors = nx.bfs_predecessors(G, source=node, depth_limit=centrality_depth_limit)
+
+                                print(f"CENTRALITY NODE: {node}, {predecessors}, {successors}")
+
+                                graph_set = set()
+                                for s in successors:
+                                    graph_set.add(s[0])
+
+                                    if isinstance(s[1], int):
+                                        graph_set.add(s[1])
+                                    else:
+                                        graph_set.update(s[1])
+
+                                for p in predecessors:
+                                    graph_set.add(p[0])
+
+                                    if isinstance(p[1], int):
+                                        graph_set.add(p[1])
+                                    else:
+                                        graph_set.update(p[1])
+
+                                SG = G.subgraph(list(graph_set))
+                                pos = nx.spring_layout(G)
+
+                                fig = self._plot_memory_network(SG, pos)
+                                plotly.io.write_html(fig=fig,
+                                                     file=f"{output_dir}/{story_id}/memory_network/{j}_{node}_memory_network.html",
+                                                     include_plotlyjs='cdn',
+                                                     include_mathjax='cdn', auto_open=False)
+
+                                cent_counter += 1
+
+
+
+                        i += 1
+
+    def add_centrality(self, G, centrality, centrality_field_name):
+        sorted_centrality = collections.OrderedDict(
+            reversed(sorted(centrality.items(), key=lambda item: item[1])))
+        for k, v in sorted_centrality.items():
+            print(centrality_field_name,k,v,)
+            G.nodes[k][f"{centrality_field_name}"] = v
+
+            print(f"{centrality_field_name}: ")
+
+        return sorted_centrality
+
+    def _plot_memory_network(self, G, pos):
+        node_x = []
+        node_y = []
+        node_colours = []
+        hover_text = []
+        for (n, data) in G.nodes(data=True):
+            x, y = pos[n]
+            node_x.append(x)
+            node_y.append(y)
+            node_colours.append(data["subset"])
+
+            if "text" in data:
+                if "title" in data:
+                    hover_text.append(
+                        f"<b>{abs(n)}</b><br><b>{data['title']}</b> <br><br> {'<br>'.join(textwrap.wrap(data['text']))}")
+                else:
+                    hover_text.append(f"<b>{abs(n)}</b><br>{'<br>'.join(textwrap.wrap(data['text']))}")
+        edge_traces = []
+        for (u, v, data) in G.edges(data=True):
+
+            if "doc_scores" in data:
+                edge_hover = f"<b>{abs(u)} --> {abs(v)} : {data['weight']}, {data['doc_scores']}</b>"
+
+            else:
+                edge_hover = f"<b>{abs(u)} --> {abs(v)}"
+
+            if "text" in data:
+                if "title" in data:
+                    text = f"<br><b>{data['title']}</b> <br><br> {'<br>'.join(textwrap.wrap(data['text']))}"
+                else:
+                    text = f"<br>{'<br>'.join(textwrap.wrap(data['text']))}"
+
+                edge_hover += f"<br>{text}"
+
+            x0, y0 = pos[u]
+            x1, y1 = pos[v]
+
+            if data["weight"] < 1.0:
+                edge_weighting  = data["probability"] * 10
+            else:
+                edge_weighting = 1.0
+
+            edge_weighting = max(1, edge_weighting )
+
+            edge_trace = go.Scatter(
+                x=[x0, x1], y=[y0, y1],
+                line=dict(width=edge_weighting, color='#888'),#, color=edge_weighting),
+                hoverinfo='text',
+                hovertext=edge_hover,
+                mode='lines')
+
+            edge_traces.append(edge_trace)
+            
+        node_trace = go.Scatter(
+            x=node_x, y=node_y,
+            mode='markers',
+            hoverinfo='text',
+            hovertext=hover_text,
+            showlegend=False)
+        node_trace.marker.color = node_colours
+        node_trace.marker.opacity = 1.0
+        node_adjacencies = []
+        for n, data in G.nodes(data=True):
+            #print(data)
+            centrality = max(data["closeness_centrality"] * 20.0, 5.0)
+
+            #centrality = max(min(len(adjacencies[1]) * 1.0, 100.00),10.0)
+
+            node_adjacencies.append(centrality)
+
+        node_trace.marker.size = node_adjacencies
+        fig = go.Figure(data=edge_traces + [node_trace],
+                        layout=go.Layout(
+                            showlegend=False,
+                            hovermode='closest',
+                            template="plotly_white",
+                            xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                            yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                        )
+        return fig
+
+    def construct_graph(self, obj, story_id):
+        G = nx.OrderedGraph(story_id=story_id)
+        previous_node = None
+        for p in obj["passages"]:
+
+            G.add_node(-int(p["seq_num"]), text=p["text"], subset=0)
+
+            if previous_node is not None:
+               G.add_edge(int(-previous_node["seq_num"]), -int(p["seq_num"]), weight=1.0, probability=1.0)
+
+            if "memory_id" in p:
+                G.add_node(int(p["memory_id"]), text=p["text"], subset=1)
+                G.add_edge(-int(p["seq_num"]), int(p["memory_id"]), weight=1.0, probability=1.0)
+
+            if "retrieved_docs" in p:
+                retrieved_docs = p["retrieved_docs"]
+
+                for doc in retrieved_docs:
+
+                    if not G.has_node(int(doc["id"])):
+                        text = ""
+                        title = ""
+                        if "text" in doc:
+                            text = doc["text"]
+
+                        if "title" in doc:
+                            title = doc["title"]
+
+                        G.add_node(int(doc["id"]), text=text, title=title, subset=2)
+
+                    G.add_edge(int(doc["id"]), -int(p["seq_num"]),
+                               weight=1.0 - doc["probability"], dot_score=doc["dot_score"], probability=doc["probability"])
+
+            previous_node = p
+        return G
+
     def create_mst_plot(self, x_edges, x_points, y_edges, y_points, id_values, text_values, cluster=None, cluster_prob=None, color_override=None):
 
         if color_override is not None:
@@ -287,16 +515,16 @@ class ClusterStories(object):
             text_list = [f"<b>{id}</b> <br><br>{t}" for id, t in
                          zip(id_values, text_values)]
 
-        tracer = go.Scatter(x=x_edges, y=y_edges,
+        edge_trace = go.Scatter(x=x_edges, y=y_edges,
                             mode='lines',
                             line=go.scatter.Line(color='#888', width=2),
                             hoverinfo='none',
                             showlegend=False)
-        tracer_marker = go.Scatter(x=list(x_points), y=list(y_points),
+        node_trace = go.Scatter(x=list(x_points), y=list(y_points),
                                    mode='markers',
                                    marker=go.scatter.Marker(size=18,
                                                             line=dict(width=1),
-                                                            colorscale="Plasma"
+                                                            colorscale="Magma"
                                                             ),
                                    # hover_name=,
                                    text=text_list,
@@ -318,7 +546,7 @@ class ClusterStories(object):
                       hovermode='closest',
                       template="plotly_white")
         
-        fig = go.Figure(dict(data=[tracer, tracer_marker], layout=layout))
+        fig = go.Figure(dict(data=[edge_trace, node_trace], layout=layout))
         return fig
 
 
